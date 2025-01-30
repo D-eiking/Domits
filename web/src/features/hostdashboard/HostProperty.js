f65e9a61d5ee3a39da3eb3aeeb1424a960fa0497
Muubii <siu7279@gmail.com>
2025-01-30 14:53:35 +0100

    HostPropety js added

---
+++ web/src/features/hostdashboard/HostProperty.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import Pages from "./Pages.js";
+
+const HostProperty = () => {
+    console.log("HostProperty Loaded");
+  return (
+    <div className="host-property">
+          <Pages />
+      <h1>Host Properties</h1>
+
+    <h2>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h2>
+ 
+    </div>
+  );
+};
+
+export default HostProperty;
