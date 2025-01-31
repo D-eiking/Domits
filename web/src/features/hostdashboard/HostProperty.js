08bc0447b7c9bca6c40a15bcc962e1d788ce24e3
Muubii <siu7279@gmail.com>
2025-01-31 16:34:04 +0100

    front end not fully done

--- web/src/features/hostdashboard/HostProperty.js
+++ web/src/features/hostdashboard/HostProperty.js
@@ -22,22 +22,47 @@ const HostProperty = () => {
       </div>
 
       <main className={styles.content}>
-      <div className="propertyHeader">
-        <Back />
-        <h1>Property-editor</h1>
+        <div className={styles.propertyHeader}>
+          <Back />
+          <h1>Property-editor</h1>
         </div>
 
-
-        <div className={styles.hostPropertyContainer}>
-          {isLoading ? (
-            <div className={styles.spinnerContainer}>
-              <img src={spinner} alt="Loading..." />
+        <div className={styles.mainSection}>
+          {/* Left side */}
+          <div className={styles.left}>
+            <div className={styles.switch}>
+              <button>Details</button>
+              <button>Rules</button>
             </div>
-          ) : (
-            <h2 className={styles.responsiveText}>
-              aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-            </h2>
-          )}
+
+            <button> 
+              <h4>Complete required steps</h4>
+              <p>Complete these final tasks to publish your listing and start getting bookings.</p>
+            </button>
+
+            <button>
+              <h4>Fotos</h4>
+              <p>Add extra photos to show people more of your accommodation.</p>
+            </button>
+
+            <button>
+              <h4>Add a new room or space</h4>
+              <p>2 bedrooms - 2 beds - 1 bathroom</p>
+            </button>
+
+            <button>
+              <h4>Title</h4>
+              <p>Tropical villa with pool</p>
+            </button>
+
+            <button>
+              <h4>Accommodation type</h4>
+              <p>Entire house - Villa</p>
+            </button>
+          </div>
+
+          {/* Right side - Just a full-size box */}
+          <div className={styles.right}></div>
         </div>
       </main>
     </div>
