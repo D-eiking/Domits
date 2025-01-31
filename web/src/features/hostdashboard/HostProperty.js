9d66800eaf8f02e683251e4f13b97a40b0af272b
Muubii <siu7279@gmail.com>
2025-01-31 13:44:43 +0100

    .

--- web/src/features/hostdashboard/HostProperty.js
+++ web/src/features/hostdashboard/HostProperty.js
@@ -1,15 +1,45 @@
 import React, { useEffect, useState } from "react";
 import Pages from "./Pages.js";
+import styles from "./HostProperty.module.css"; 
+import spinner from "../../images/spinnner.gif";
+import Back from '@mui/icons-material/KeyboardBackspace';
 
 const HostProperty = () => {
-    console.log("HostProperty Loaded");
-  return (
-    <div className="host-property">
-          <Pages />
-      <h1>Host Properties</h1>
+  console.log("HostProperty Loaded");
 
-    <h2>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h2>
- 
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.sidebar}>
+        <Pages />
+      </div>
+
+      <main className={styles.content}>
+      <div className="propertyHeader">
+        <Back />
+        <h1>Property-editor</h1>
+        </div>
+
+
+        <div className={styles.hostPropertyContainer}>
+          {isLoading ? (
+            <div className={styles.spinnerContainer}>
+              <img src={spinner} alt="Loading..." />
+            </div>
+          ) : (
+            <h2 className={styles.responsiveText}>
+              aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            </h2>
+          )}
+        </div>
+      </main>
     </div>
   );
 };
